20de0560751e1c7eed265dbfe28e3e967d5902b9
wangxuguo <slogen.wang@tron.network>
2024-04-11 16:58:14 +0800

    add mnemonic import test

--- core/src/test/java/org/tron/WalletCoreUnitTest.java
+++ core/src/test/java/org/tron/WalletCoreUnitTest.java
@@ -20,6 +20,9 @@ public class WalletCoreUnitTest {
     private static final String PRIVETE_KEY = "8e0de1652bb518de63f8a1b8b6d3ad195c069024abe59e4b31fc8a70e124879a";
     private static final String WALLET_ADDRESS = "TLVoXnzfyDmwRDwHwS1yZANjjDWFJ3HwrB";
 
+    private static final String MNEMONIC = "pear flush between small sure month snow morning elite solid soap stable";
+    private static final String WALLET_MNEMONIC_ADDRESS = "TYEQEtgNLqFg11TK39ZSMX3a3mbYTGkkYm";
+
     @Test
     public void createWallet() throws CipherException, InvalidPasswordException, DuplicateNameException, InvalidNameException {
         Wallet testWallet = new Wallet(true);
@@ -31,6 +34,16 @@ public class WalletCoreUnitTest {
     }
 
 
+    @Test
+    public void importWalletWithMnemonic() {
+        Wallet testWallet = new Wallet(I_TYPE.MNEMONIC, MNEMONIC);
+        testWallet.setWalletName(WALLET_NAME);
+        testWallet.setCreateType(Parameter.CreateWalletType.TYPE_IMPORT_MNEMONIC_HD);
+        testWallet.setCreateTime(System.currentTimeMillis());
+        System.out.println("Address = " + testWallet.getAddress());
+        Assert.assertTrue(testWallet.getAddress() != null);
+        Assert.assertEquals(WALLET_MNEMONIC_ADDRESS, testWallet.getAddress());
+    }
     @Test
     public void importWalletWithPrivateKey() {
         Wallet testWallet = new Wallet(I_TYPE.PRIVATE, PRIVETE_KEY);
