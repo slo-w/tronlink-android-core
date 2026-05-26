12508ed1cceaab953fe7e1dbe8c81769fcddbd3b
SlashWang <59639229+JSA-coder@users.noreply.github.com>
2026-05-26 11:52:39 +0800

    [fix core review]: 0526 Q04

--- core/src/main/java/org/tron/metrics/reporter/ReportStatConfig.java
+++ core/src/main/java/org/tron/metrics/reporter/ReportStatConfig.java
@@ -4,9 +4,9 @@ package org.tron.metrics.reporter;
  * Data reporting configuration
  */
 public class ReportStatConfig {
-    
-    public static boolean ENABLE_RETRY = true;
-    public static boolean isFirstAssetDataLoad = true;
+
+    public static volatile boolean ENABLE_RETRY = true;
+    public static volatile boolean isFirstAssetDataLoad = true;
     public static final boolean DEFAULT_PLAIN_STATUS = false;
     public static final String LOG_TAG = "ReporterManager";
     public static final String VERSION_X = "V1X";
